fa203c012666951c990ecf20e01125cb0ab082cd
Daniel <vdguevara@uninorte.edu.co>
2018-11-07 10:57:37 -0500

    fox sprite added

--- src/Game.js
+++ src/Game.js
@@ -4,15 +4,14 @@ export default class Game extends Phaser.Scene {
     }
     
     preload(){
-
+        this.load.spritesheet('fox', 'assets/sprites/sprite_fox.png', {frameWidth: 32, frameHeight: 60});
     }
     
     create(){
-
+        this.fox = this.physics.add.sprite('fox', 0, 0);
     }
 
     update(){
         
     }
-
 }
--- src/MainMenu.js
+++ src/MainMenu.js
@@ -1,24 +1,19 @@
 export default class MainMenu extends Phaser.Scene {
-
+    
     constructor() {
-        super({ key: 'MainMenu', active: true });
+        super({key: 'MainMenu', active: true});
     }
 
     preload() {
-
+        
     }
-
 
     create() {
         this.add.text(100, 100, "BomberWeeb", { font: "100px Courier", fill: "#AED7F1" });
     }
-    //let.graphics = this.add.graphics();
-    //graphics.fillStyle(0xff9999,1);
-
-
-
-
+    
     update() {
+
     }
 
 }
