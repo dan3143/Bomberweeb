ad069ce2300585a66740aaae1c52a0f68c1fa70d
Daniel <vdguevara@uninorte.edu.co>
2018-11-07 12:20:25 -0500

    main menu added, trying sprites

--- src/Game.js
+++ src/Game.js
@@ -1,17 +1,70 @@
 export default class Game extends Phaser.Scene {
+    
     constructor(){
         super({key: 'Game'});
+        this.speed = 200;
     }
     
     preload(){
-        this.load.spritesheet('fox', 'assets/sprites/sprite_fox.png', {frameWidth: 32, frameHeight: 60});
+        this.load.spritesheet('fox', '/../assets/sprites/sprite_fox.png', {frameWidth: 32, frameHeight: 60});
+        
     }
     
     create(){
-        this.fox = this.physics.add.sprite('fox', 0, 0);
-    }
-
-    update(){
+        this.cursors = this.input.keyboard.createCursorKeys();
+        this.fox = this.physics.add.sprite(100, 100, 'fox');
         
+        this.fox.setCollideWorldBounds(true);
+
+        this.anims.create({
+            key: 'down',
+            frames: this.anims.generateFrameNumbers('fox', { start: 8, end: 11 }),
+            frameRate: 5,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'up',
+            frames: this.anims.generateFrameNumbers('fox', { start: 0, end: 3 }),
+            frameRate: 5,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('fox', { start: 12, end: 15 }),
+            frameRate: 5,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('fox', { start: 4, end: 7 }),
+            frameRate: 5,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'steady',
+            frames: [{key:'fox', frame:0}],
+            frameRate: 5,
+            repeat: -1
+        });
+    }
+    
+    update(){
+        this.fox.setVelocity(0);
+        
+        if (this.cursors.left.isDown){
+            this.fox.setVelocityX(-this.speed);
+            this.fox.anims.play('left', true);
+        }else if (this.cursors.right.isDown){
+            this.fox.setVelocityX(this.speed);
+            this.fox.anims.play('right', true);
+        }else if (this.cursors.up.isDown){
+            this.fox.setVelocityY(-this.speed);
+            this.fox.anims.play('down', true);
+        }else if (this.cursors.down.isDown){
+            this.fox.setVelocityY(this.speed);
+            this.fox.anims.play('up', true);
+        }else{
+            this.fox.anims.play('steady', true);
+        }
     }
 }
--- src/MainMenu.js
+++ src/MainMenu.js
@@ -1,19 +1,57 @@
+import Game from './Game.js';
+
 export default class MainMenu extends Phaser.Scene {
     
     constructor() {
         super({key: 'MainMenu', active: true});
+        this.current_element = -1;
+        this.last_element = 1;
     }
 
     preload() {
-        
+        this.load.image('megu', 'assets/megumin.png');
     }
 
     create() {
-        this.add.text(100, 100, "BomberWeeb", { font: "100px Courier", fill: "#AED7F1" });
+        let self = this;
+        this.add.text(90, 100, "BomberWeeb", { font: "100px Courier", fill: "#AED7F1" });
+        this.startGame = this.add.text(100, 250, 'Comenzar juego', {font: "25px Courier"}).setInteractive({ useHandCursor: true  } );
+        this.instructions = this.add.text(100, 300, 'Instrucciones', {font: "25px Courier"}).setInteractive({ useHandCursor: true  } );
+        this.startGame.on('pointerover', function(){
+            self.startGame.setText("> Comenzar Juego");
+        });
+
+        this.startGame.on('pointerout', function(){
+            self.startGame.setText("Comenzar Juego");
+            self.startGame.setColor("#FFFFFF");
+        });
+
+        this.startGame.on('pointerdown', function(){
+            self.startGame.setColor("#000000");
+            self.scene.start('Game', new Game);
+        });
+
+        this.startGame.on('pointerup', function(){
+            self.startGame.setColor("#FFFFFF");
+        })
+
+        this.instructions.on('pointerover', function(){
+            self.instructions.setText("> Instrucciones");
+        });
+
+        this.instructions.on('pointerout', function(){
+            self.instructions.setText("Instrucciones");
+            self.instructions.setColor("#FFFFFF");
+        });
+        
+        this.instructions.on('pointerdown', function(){
+            self.instructions.setColor("#000000");
+        });
+        this.instructions.on('pointerup', function(){
+            self.instructions.setColor("#FFFFFF");
+        });
     }
-    
+
     update() {
-
     }
-
 }
