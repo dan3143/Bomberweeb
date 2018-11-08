6df06c2e61b16ae9ac205d4301581686218f9b0e
Daniel <vdguevara@uninorte.edu.co>
2018-11-08 16:55:24 -0500

    Lógica de asignación de personaje terminada

--- server.js
+++ server.js
@@ -16,47 +16,45 @@ app.get('/', function(req, res){
 });    
 
 io.on('connect', function(socket){
-    
     if (io.engine.clientsCount > connections_limit){
         console.log('Maximun players exceeded');
         console.log('Disconnecting...');
         socket.disconnect();
         return;
     }
-
     socket.on('playerConnected', function(){
         socket.on('disconnect', function(){
             socket.broadcast.emit('playerDisconnected', socket.id);
             delete players[socket.id];
             console.log('Player disconnected');
         });
+        
         console.log("A player entered the game");
         socket.emit('connectedPlayers', players);   
-        var playerNumber = 0;
-        if (player1 === false){
-            player1 = true;
-            playerNumber = 1;
-        }else if (player2 === false){
-            player2 = true;
-            playerNumber = 2;
-        }else{
-            player1 = true;
-            player2 = false;
-            playerNumber = 1;
-        }
+        
         players[socket.id] = {
             x: Math.floor(Math.random() * 500) + 15,
             y: Math.floor(Math.random() * 500) + 15,
-            playerNumber: playerNumber,
+            playerNumber: getPlayerNumber(),
             playerId: socket.id
         }; 
-
         io.sockets.emit('newPlayer', players[socket.id]);
-        
     });
-
-    
 });
+
+function getPlayerNumber(){
+    if (player1 === false){
+        player1 = true;
+        return 1;
+    }else if (player2 === false){
+        player2 = true;
+        return 2;
+    }else{
+        player1 = true;
+        player2 = false;
+        return 1;
+    }
+}
 
 server.listen(8080, function(){
     console.log("Server on, listening to port 8080");
